fcb2af5ba3fc232b2d2797397cdb7923b8162250
ProudMay <mehdi79nekouei@gmail.com>
2025-11-07 18:44:35 +0330

    feat [ add game Controller module ] add 2 players with markers. current player for turn . game over situation .

--- scripts.js
+++ scripts.js
@@ -23,5 +23,27 @@ function player (name,marker){
 
 return {getName , getMarker };
 }
-const player1 = player('dashaq', 'x');
-console.log(`Hi im ${player1.getName()} and my marker is ${player1.getMarker()}`);
+const newPlayer = player('dashaq', 'x');
+console.log(`Hi im ${newPlayer.getName()} and my marker is ${newPlayer.getMarker()}`);
+
+function gameController = () => {
+    const player1 = player('player 1' ,'X');
+    const player2 = player('player 2' ,'O');
+    let currentPlayer = player1;
+    let gameOver = false;
+
+    const playRound = (index) => {
+        if(gameOver) return;
+        const board = gameBoard.gameBoard();
+
+        if(board[index] !=="") return;
+        gameBoard.updateBoard(index,currentPlayer.marker);
+        
+        if(checkWinner(board)){
+            gameOver = true;
+            console.log(`${currentPlayer} Wins the battle !`);
+            return
+        }
+        
+    }
+}
