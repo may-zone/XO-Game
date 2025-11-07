0b7f2c53a68beecb1f621a0f3047dcf441762976
ProudMay <mehdi79nekouei@gmail.com>
2025-11-07 17:45:26 +0330

    fix [ gameBoard module syntax errors ]

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 function gameBoard (){
     let board = ["" , "", "","", "", "", "", "", ""];
     const getBoard = () => board;
-    const setMark(index,marker){
+    const setMark = (index,marker) =>{
         if(board[index]===""){
             board[index] = marker;
             return true;
         }
         return false;
     }
-    const resetBoard() {
-        for (let i=0 ; i< board,length ; i++){
+    const resetBoard =() => {
+        for (let i=0 ; i< board.length ; i++){
             board[i] = "";
         };
     };
     return { getBoard, setMark, resetBoard};
-};
+}
 console.log("Hi")
